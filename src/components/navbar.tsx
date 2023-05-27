91f20117f410174b0ffc2721ad81933c9df587ec
Allyson1602 <aathyrson@yahoo.com.br>
2023-05-27 20:43:24 -0300

    Criação do topbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,12 @@
 import logoMenu from "../assets/images/logo-menu.svg";
-import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, MenuItem, MenuList, Paper, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
-import { styled, alpha } from '@mui/material/styles';
+import { styled, alpha, useTheme } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
+import { ContentCut } from "@mui/icons-material";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -24,8 +26,8 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
     position: 'absolute',
-    right: "0",
-    top: "0",
+    right: '0',
+    top: '0',
     pointerEvents: 'none',
     display: 'flex',
     alignItems: 'center',
@@ -36,7 +38,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 0, 1, 1),
-        // vertical padding + font size from searchIcon
         paddingRight: `calc(1em + ${theme.spacing(4)})`,
         transition: theme.transitions.create('width'),
         width: '100%',
@@ -48,6 +49,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 const Navbar: React.FC = () => {
     const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+    const [openDrawer, setOpenDrawer] = useState(false);
 
     return (
         <AppBar position="static">
@@ -60,6 +62,24 @@ const Navbar: React.FC = () => {
                         <img src={logoMenu} alt="logo" style={{ width: '50px' }} />
                     </div>
                     
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <Button
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >escola</Button>
+                        
+                        <Button
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >escola</Button>
+                        
+                        <Button
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >escola</Button>
+                        
+                        <Button
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >escola</Button>
+                    </Box>
+                    
                     <Search>
                         <StyledInputBase placeholder="procurar..." />
 
@@ -68,69 +88,106 @@ const Navbar: React.FC = () => {
                         </SearchIconWrapper>
                     </Search>
                     
-                    <div>
-                        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >escola</Button>
-                            
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >escola</Button>
-                            
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >escola</Button>
-                            
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >escola</Button>
-                        </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            color="inherit"
+                            onClick={() => setOpenDrawer(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
 
-                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                // onClose={handleCloseNavMenu}
+                        <Drawer
+                            anchor={'right'}
+                            open={openDrawer}
+                            onClose={() => setOpenDrawer(false)}
+                        >
+                            <Box
                                 sx={{
-                                    display: { xs: 'block', md: 'none' },
+                                    width: "100vw",
+                                    height: "100%",
+                                    backgroundColor: (theme) => theme.palette.primary.light
                                 }}
                             >
-                                    <MenuItem>
-                                        <Typography textAlign="center">escola</Typography>
-                                    </MenuItem>
+                                <IconButton
+                                    onClick={() => setOpenDrawer(false)}
+                                    sx={{
+                                        position: 'absolute',
+                                        right: 8,
+                                        top: 8,
+                                        color: (theme) => theme.palette.grey[500],
+                                    }}
+                                >
+                                    <CloseIcon />
+                                </IconButton>
 
+                                <MenuList
+                                    sx={{
+                                        marginTop: '2rem',
+                                    }}
+                                >
                                     <MenuItem>
-                                        <Typography textAlign="center">escola</Typography>
-                                    </MenuItem>
+                                        <ListItemIcon>
+                                            <ContentCut fontSize="small" />
+                                        </ListItemIcon>
 
-                                    <MenuItem>
-                                        <Typography textAlign="center">escola</Typography>
+                                        <ListItemText>Escola</ListItemText>
                                     </MenuItem>
+                                    
+                                    <MenuItem>
+                                        <ListItemIcon>
+                                            <ContentCut fontSize="small" />
+                                        </ListItemIcon>
 
-                                    <MenuItem>
-                                        <Typography textAlign="center">escola</Typography>
+                                        <ListItemText>Escola</ListItemText>
                                     </MenuItem>
-                            </Menu>
-                        </Box>
-                    </div>
+                                    
+                                    <MenuItem>
+                                        <ListItemIcon>
+                                            <ContentCut fontSize="small" />
+                                        </ListItemIcon>
+
+                                        <ListItemText>Escola</ListItemText>
+                                    </MenuItem>
+                                </MenuList>
+                            </Box>
+                        </Drawer>
+
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            // onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                                <MenuItem>
+                                    <Typography textAlign="center">escola</Typography>
+                                </MenuItem>
+
+                                <MenuItem>
+                                    <Typography textAlign="center">escola</Typography>
+                                </MenuItem>
+
+                                <MenuItem>
+                                    <Typography textAlign="center">escola</Typography>
+                                </MenuItem>
+
+                                <MenuItem>
+                                    <Typography textAlign="center">escola</Typography>
+                                </MenuItem>
+                        </Menu>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
